8726dd0bab5ed503ddac8bac1e35d185d7527cac
Ricky <mr.emps94@gmail.com>
2021-02-06 10:53:57 +0100

    test 1 to 99 print string if divisible for 3 and 7

---
+++ buffer.js
@@ -0,0 +1,21 @@
+var i;
+var string = " ";
+
+for(i=1;i<100;i++){
+    
+    string = "";
+    
+    if(i%3===0){
+        string +=" Open ";
+    }
+    if(i%7===0){
+        string += " Source ";
+    }
+
+    if(string.length>3)
+      console.log(string);
+    else
+      console.log(i);
+    
+
+}
